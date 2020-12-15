0fab645a6fe0dade25d0ba3651c565a8e4770cba
yalcinozer <48597826+yalcinozer@users.noreply.github.com>
2020-12-15 23:28:09 +0300

    Revised

---
+++ precache-manifest.f8350ef72a012286c02ab430f5de871c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895ca4dc93e3953826659f8f0f69f988",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5000773acee050a5d919",
+    "url": "/static/css/main.3fd260d6.chunk.css"
+  },
+  {
+    "revision": "440360c4aa7bbbeb1420",
+    "url": "/static/js/2.705a21af.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.705a21af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5000773acee050a5d919",
+    "url": "/static/js/main.c5bedd7b.chunk.js"
+  },
+  {
+    "revision": "1bda14d20000ed7a6c56",
+    "url": "/static/js/runtime-main.9fdf43a0.js"
+  },
+  {
+    "revision": "428698b3dd2dfd351ddad483fc8924dc",
+    "url": "/static/media/1a2b.428698b3.png"
+  },
+  {
+    "revision": "4a07a75ca9a293f2821500fc1afc1f81",
+    "url": "/static/media/JandaManateeSolid.4a07a75c.ttf"
+  },
+  {
+    "revision": "6e1159c9b0afc5fd38464558a7ee54eb",
+    "url": "/static/media/esmat-temiz.6e1159c9.svg"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/static/media/google-play-badge-en.db9b21a1.png"
+  },
+  {
+    "revision": "761b5d2eed40cc8c4564a6e51b151731",
+    "url": "/static/media/logo.761b5d2e.png"
+  },
+  {
+    "revision": "1385b032d281123ffb181f3b18af0ac8",
+    "url": "/static/media/smirking.1385b032.png"
+  },
+  {
+    "revision": "70e6deddd4ef7bedfbed42270cad3c91",
+    "url": "/static/media/yalcin.70e6dedd.jpg"
+  }
+]);
